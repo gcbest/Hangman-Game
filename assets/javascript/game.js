2dc3dcc5730184dab498d199f587cd3521174e96
Godfrey Best <godfreybest@Godfrey-Bests-MacBook.local>
2016-08-25 00:46:48 -0400

    working hangman game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,40 @@
-var word_options = ["dog", "car", "me", "alpha", "redman"];
+var word_options = ["petra", "christ", "great", "machu", "colosseum", "chichen", "taj"];
+
+var winning_pics = [
+				'<img src="assets/images/petra.jpg" alt="petra">',
+				'<img src="assets/images/christ.jpg" alt="christ">',
+				'<img src="assets/images/great.jpg" alt="great">',
+				'<img src="assets/images/machu.jpg" alt="machu">',
+				'<img src="assets/images/colosseum.jpg" alt="colosseum">',
+				'<img src="assets/images/chichen.jpg" alt="chichen">',
+				'<img src="assets/images/taj.jpg" alt="taj">'
+			];
+
+var wonder = [
+			"Petra", 
+			"Christ the Redeemer", 
+			"Great Wall of China",
+			"Machu Pichu",
+			"The Colosseum",
+			"Chichen Itza",
+			"Taj Mahal"
+		];
+
+
 var letters_guessed = [];
 var wins = 0;
 var guesses_remaining = 15;
-var current_word = word_options[Math.round(Math.random() * (word_options.length - 1))];
+var random_index = Math.round(Math.random() * (word_options.length - 1));
+var current_word = word_options[random_index];
+var current_pic = winning_pics[random_index];
 var blanks = "";
 var blanks_with_spaces = "";
+
 
 function print_blanks() {
 	for (i=0; i < current_word.length; i++) {
 		blanks += "_";
 	}
-	console.log(blanks);
 	return blanks;	
 }
 
@@ -18,14 +42,13 @@ function addSpaces(str) {
 	//create an array of the blanks
 	var with_spaces = str.split("");
 	
-	//traverse the array and push to the array a space at the odd indices
+	//traverse the array and add to the array a space at the odd indices
 	for (var i=1; i < with_spaces.length; i+=2) {
 		with_spaces.splice(i, 0, " ");
 	}
 	
 	//set the array to point to itself with the spaces now added
 	with_spaces = with_spaces;
-	console.log(with_spaces);
 	//rejoin the array into a string
 	var new_string = with_spaces.join("");
 	
@@ -38,7 +61,6 @@ function replaceString(str,char) {
  
   //split up the blanks string
   var blanks_array = str.split("");
-  console.log(blanks_array);
   //split up the current_word string
   var word_array = current_word.split("");
   /*
@@ -51,15 +73,12 @@ function replaceString(str,char) {
   	}
 
   }
-  console.log("This is the blanks now the array " + blanks_array);
   //update the blanks_array so that it points to array with the newly inserted chars
   blanks_array = blanks_array;
 
   //rejoin the blanks into string
   blanks = blanks_array.join("");
-  console.log("This is the new string " + blanks);
 }
-
 
 function play() {
 	print_blanks();
@@ -67,39 +86,38 @@ function play() {
 	document.querySelector("#guess_count").innerHTML = guesses_remaining;
 	document.querySelector("#letters_guessed").innerHTML = letters_guessed;
 	// addSpaces(blanks);
-	console.log(current_word);
 	document.onkeyup = function() {
 		
 		var current_guess = String.fromCharCode(event.keyCode).toLowerCase();
 		
+
 		if (guesses_remaining > 1) {
 			if (current_word.indexOf(current_guess) > -1) {
-				var blanks2 = replaceString(blanks, current_guess);
+				replaceString(blanks, current_guess);
 				addSpaces(blanks);
 			}
 
+			//No more blanks left, therefore user wins
 			if (blanks.indexOf("_") < 0) {
 				wins++;
 				guesses_remaining = 15;
 				letters_guessed = [];
 				blanks = "";
+				$('#wonder_name').html(wonder[random_index]);
+				$('#winner_pic').html(winning_pics[random_index]);
+				alert("You got it! The word was " + current_word);
+				
 				document.querySelector("#win_total").innerHTML = wins;
-				current_word = word_options[Math.round(Math.random() * (word_options.length - 1))];
-				/* 
-				OutPut A Winner PAGE!!!!
-				The Last word was Car etc. Image of car
-				*/
+				random_index = Math.round(Math.random() * (word_options.length - 1));
+				current_word = word_options[random_index];
 				play();
 			}
 			
 
-			// PRETTIFY letters guessed output
-			
 			else {
 				if (letters_guessed.indexOf(current_guess) < 0 && blanks.indexOf(current_guess) < 0) {
 					letters_guessed.push(current_guess);
 					guesses_remaining--;
-					console.log(guesses_remaining);
 					document.querySelector("#guess_count").innerHTML = guesses_remaining;
 					document.querySelector("#letters_guessed").innerHTML = letters_guessed;
 				}
@@ -107,15 +125,18 @@ function play() {
 		}
 
 		else {
+			alert("Womp Womp.....Let's try a new word");
 			guesses_remaining = 15;
+			winning = 
 			letters_guessed = [];
 			blanks = "";
-			current_word = word_options[Math.round(Math.random() * (word_options.length - 1))];
+			current_word = word_options[random_index];
 			play();
-
 		}		
 	}	
 }
+			
+	
 
 play();
 
